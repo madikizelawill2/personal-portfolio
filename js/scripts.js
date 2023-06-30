c16ee5e5101e67b169299aacc05abef6756ee2cc
Will <madikizelawill2@gmail.com>
2023-06-30 15:45:38 +0200

    [ADDED] function for sending emails on the contact form

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,18 @@
-/*!
-* Start Bootstrap - Personal v1.0.1 (https://startbootstrap.com/template-overviews/personal)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-personal/blob/master/LICENSE)
-*/
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
+
+
+function sendEmail(){
+    Email.send({
+        Host : "smtp.gmail.com",
+        Username : "madikizelawill2@gmail.com",
+        Password : "password",
+        To : 'madikizelawill2@gmail.com',
+        From : document.getElementById().value,
+        Subject : "Contact from portfolio site",
+        Body : "Name: " + document.getElementById().value
+            + "<br> Email: " + document.getElementById().value
+            + "<br> Phone Number: " + document.getElementById().value
+            + "<br> Message: " + document.getElementById().value
+    }).then(
+      message => alert("Message sent successfully!")
+    );
+}
