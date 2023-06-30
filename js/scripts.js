62c73b70633fea1fe32aa3560537d0af078edbb7
Will <madikizelawill2@gmail.com>
2023-06-30 22:48:08 +0200

    [UPDATE] worked on email server issues

--- js/scripts.js
+++ js/scripts.js
@@ -2,16 +2,16 @@
 
 function sendEmail(){
     Email.send({
-        Host : "smtp.gmail.com",
+        Host : "smtp.elasticemail.com",
         Username : "madikizelawill2@gmail.com",
-        Password : "password",
+        Password : "B0B03A16EED4B8C21E8B521C829C6B618588",
         To : 'madikizelawill2@gmail.com',
-        From : document.getElementById().value,
+        From : document.getElementById("email").value,
         Subject : "Contact from portfolio site",
-        Body : "Name: " + document.getElementById().value
-            + "<br> Email: " + document.getElementById().value
-            + "<br> Phone Number: " + document.getElementById().value
-            + "<br> Message: " + document.getElementById().value
+        Body : "Name: " + document.getElementById("name").value
+            + "<br> Email: " + document.getElementById("email").value
+            + "<br> Phone Number: " + document.getElementById("phone").value
+            + "<br> Message: " + document.getElementById("message").value,
     }).then(
       message => alert("Message sent successfully!")
     );
